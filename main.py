fda6459f17912485a66df988e6f0562e7bacf652
AntonDawinan <antondawinan72@gmail.com>
2024-08-29 18:46:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyfiglet
+
+text=pyfiglet.print_figlet(text="Anton",font="Alpha")
+
+text=pyfiglet.print_figlet(text="Sebastian C.",font="Alpha")
+
+text=pyfiglet.print_figlet(text="Dawinan",font="Alpha")
+
+print(text) 
